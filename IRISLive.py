0b71e187d12a5742164488bb23757953d57af5ab
Jkookers <70289625+Jkookers@users.noreply.github.com>
2020-08-31 11:16:59 -0700

    New IRISLive file that uses threading
    Improves the latency of the application and uses threading to produce plots.

--- IRISLive.py
+++ IRISLive.py
@@ -10,15 +10,22 @@ of the 3 channels.
 When finished, press "stop" and a log file titled "DipLog.txt" will be
 generated in the parent folder.
 
+V1.1 Now uses threading to improve response from the app and updated the degree
+of the "tiltmeters"
+
 NOTE TO USERS: Threading is not perfect! To exit from the application, DO NOT
-CLICK THE 'X' button! Please hit 'Stop' before exiting. There will be lag in
-in the application.
+CLICK THE 'X' button! Please hit 'Stop' before exiting. Stop needs to be
+pressed to save the log file and stop the loop. Failure to do so will see the
+loop continue in the background.
 """
 
 from PyQt5 import QtGui, QtCore, QtWidgets
+from PyQt5.QtCore import pyqtSignal, QThread
 
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
+
+
 
 import numpy as np
 from obspy.clients.fdsn import Client
@@ -28,18 +35,110 @@ client = Client("IRIS")
 
 import matplotlib.pyplot as plt
 
-class Ui_Form(object):
+
+class GetLiveThread(QThread):
     
+    #   Signals from thread class to feed back into the main app
+    updateWave = pyqtSignal(Stream,float,float,float)
+    updateLogger = pyqtSignal(str)
+    
+    #   Define initial variables 
+    def __init__(self, starter, URLGrabZ, URLGrabN, URLGrabE):
+        super().__init__()
+        self.starter = starter
+        self.URLGrabZ = URLGrabZ
+        self.URLGrabN = URLGrabN
+        self.URLGrabE = URLGrabE
+    
+    #   Run for thread
+    def run(self):
+        #   Log file header
+        logger = "Time(UTC)\t\t\t\tZ-Channel Dip\t\tN-Channel Dip\t\tE-Channel Dip"
+        
+        #   Retrieve the 3 URLs
+        fullURL = [str(self.URLGrabZ.text()),
+                   str(self.URLGrabN.text()),
+                   str(self.URLGrabE.text())]
+        
+        #   Parse the front and back ends of the URL to single out the changing time
+        #   Will create a list of lists going z channel, n channel, and e channel
+        #   each sublist will be the front of the URL then the end of the URL
+        URLParse = [[fullURL[0][:fullURL[0].find('start=')],fullURL[0][(fullURL[0].rfind('&end=') + 24):]],
+                    [fullURL[1][:fullURL[1].find('start=')],fullURL[1][(fullURL[1].rfind('&end=') + 24):]],
+                    [fullURL[2][:fullURL[2].find('start=')],fullURL[2][(fullURL[2].rfind('&end=') + 24):]]]
+        
+        #   Get the channel names
+        chans = [(fullURL[0][fullURL[0].find('cha=')+4:])[0:3], #z channel
+                 (fullURL[1][fullURL[1].find('cha=')+4:])[0:3], #n channel
+                 (fullURL[2][fullURL[2].find('cha=')+4:])[0:3]] #e channel
+        
+        #   Get the current time of the button press
+        gettimeNow = datetime.datetime.now()-datetime.timedelta(seconds=1)
+
+        
+        #   Generate the time strings for the URL
+        #   Time is 1 second behind real time to allow for buffering
+        t_start = str((gettimeNow-datetime.timedelta(seconds=1)).date())+'T'+str((gettimeNow-datetime.timedelta(seconds=1)).time())
+        t = str(gettimeNow.date())+'T'+str(gettimeNow.time())
+        
+        #   Begin while loop for main data
+        while self.starter == True:
+            #   Combine the string for the URL
+            timestring = 'start=' + t_start + '&end=' + t
+            
+            #   Make the stream variable
+            st = Stream()
+                  
+            #   Get data
+            for i in range(3):        
+                url = URLParse[i][0] + timestring + URLParse[i][1]
+                st.append(read(url)[0])
+                print(chans[i])
+        
+            
+            #   Get the mean for gravity from the channels
+            Zchan = np.mean(st.select(channel=chans[0])[0].data)
+            Nchan =  np.mean(st.select(channel=chans[1])[0].data)
+            Echan =  np.mean(st.select(channel=chans[2])[0].data)
+            
+            g = np.sqrt(Zchan**2+Nchan**2+Echan**2)
+
+            
+            # Get the current angles for each channel
+            alpha_N = np.arcsin(Nchan/g)*180/np.pi
+            alpha_E = np.arcsin(Echan/g)*180/np.pi
+            alpha_Z = np.arccos(Zchan/g)*180/np.pi
+            print(t + " Z: " + str(alpha_Z) + " N: " + str(alpha_N) + " E: " + str(alpha_E))
+            
+            #   Generate the logger text
+            logger += "\n" + t + '\t\t' + str('%.5f' % (round(alpha_Z,5))) + '\t\t' + str('%.5f' % (round(alpha_N,5))) + '\t\t' + str('%.5f' % (round(alpha_E,5)))
+            
+            #   Send signal
+            self.updateWave.emit(st, alpha_Z, alpha_N, alpha_E)
+            self.updateLogger.emit(logger)
+            
+            #   Set newest time for update loop
+            gettimeNow = datetime.datetime.now()-datetime.timedelta(seconds=1)
+
+            t_start = t
+            t = str(gettimeNow.date())+'T'+str(gettimeNow.time())
+
+
+#   Main GUI
+class Ui_Form(object):
     def setupUi(self, Form):
         
-        self.startstop = 0
-        
+        #   Send to write variables
+        self.startstop = False
+        self.writer = False
+               
         #   Define main application
         Form.setObjectName("Form")
         Form.resize(1500, 800)
         self.gridLayout = QtWidgets.QGridLayout(Form)
         self.gridLayout.setObjectName("gridLayout")
         self.groupBox_3 = QtWidgets.QGroupBox(Form)
+        
         font = QtGui.QFont()
         font.setFamily("Arial")
         font.setPointSize(12)
@@ -187,34 +286,34 @@ class Ui_Form(object):
         self.ax1.plot(0,1)
         self.ax1.set_theta_direction(-1)
         self.ax1.set_theta_zero_location("E")
-        self.ax1.set_thetamin(-90)
-        self.ax1.set_thetamax(90)
-        self.ax1.set_xticks(np.linspace((-1*(np.pi))/2,(np.pi)/2, 13))
+        self.ax1.set_thetamin(-15)
+        self.ax1.set_thetamax(15)
+        self.ax1.set_xticks(np.linspace((-1*(np.pi))/12,(np.pi)/12, 7))
         self.ax1.set_yticklabels([])
         self.ax1.set_title("Z-Channel Dip", pad = 20, size = 18)
-        self.ax1.set_xticklabels(['-90'+self.degS + '\n(Up)','-75'+self.degS,'-60'+self.degS,'-45'+self.degS,'-30'+self.degS,'-15'+self.degS,'0'+self.degS + "\n(Seafloor)",'15'+self.degS,'30'+self.degS,'45'+self.degS,'60'+self.degS,'75'+self.degS,'90'+self.degS +'\n(Down)'], size = 10)
+        self.ax1.set_xticklabels(['-15'+self.degS + ' (Up)','-10'+self.degS,'-5'+self.degS,'0'+self.degS,'5'+self.degS,'10'+self.degS,'15'+self.degS + ' (Down)'], size = 10)
         
         #   N-channel tiltmeter
         self.ax2.plot(0,1)
         self.ax2.set_theta_direction(-1)
         self.ax2.set_theta_zero_location("E")
-        self.ax2.set_thetamin(-90)
-        self.ax2.set_thetamax(90)
-        self.ax2.set_xticks(np.linspace((-1*(np.pi))/2,(np.pi)/2, 13))
+        self.ax2.set_thetamin(-15)
+        self.ax2.set_thetamax(15)
+        self.ax2.set_xticks(np.linspace((-1*(np.pi))/12,(np.pi)/12, 7))
         self.ax2.set_yticklabels([])
         self.ax2.set_title("N-Channel Dip", pad = 20, size = 18)
-        self.ax2.set_xticklabels(['-90'+self.degS + '\n(Up)','-75'+self.degS,'-60'+self.degS,'-45'+self.degS,'-30'+self.degS,'-15'+self.degS,'0'+self.degS + "\n(Seafloor)",'15'+self.degS,'30'+self.degS,'45'+self.degS,'60'+self.degS,'75'+self.degS,'90'+self.degS +'\n(Down)'], size = 10)
+        self.ax2.set_xticklabels(['-15'+self.degS + ' (Up)','-10'+self.degS,'-5'+self.degS,'0'+self.degS,'5'+self.degS,'10'+self.degS,'15'+self.degS + ' (Down)'], size = 10)
         
         #   E-channel tiltmeter
         self.ax3.plot(0,1)
         self.ax3.set_theta_direction(-1)
         self.ax3.set_theta_zero_location("E")
-        self.ax3.set_thetamin(-90)
-        self.ax3.set_thetamax(90)
-        self.ax3.set_xticks(np.linspace((-1*(np.pi))/2,(np.pi)/2, 13))
+        self.ax3.set_thetamin(-15)
+        self.ax3.set_thetamax(15)
+        self.ax3.set_xticks(np.linspace((-1*(np.pi))/12,(np.pi)/12, 7))
         self.ax3.set_yticklabels([])
         self.ax3.set_title("E-Channel Dip", pad = 20, size = 18)
-        self.ax3.set_xticklabels(['-90'+self.degS + '\n(Up)','-75'+self.degS,'-60'+self.degS,'-45'+self.degS,'-30'+self.degS,'-15'+self.degS,'0'+self.degS + "\n(Seafloor)",'15'+self.degS,'30'+self.degS,'45'+self.degS,'60'+self.degS,'75'+self.degS,'90'+self.degS +'\n(Down)'], size = 10)
+        self.ax3.set_xticklabels(['-15'+self.degS + ' (Up)','-10'+self.degS,'-5'+self.degS,'0'+self.degS,'5'+self.degS,'10'+self.degS,'15'+self.degS + ' (Down)'], size = 10)
         
         
         #   Live Tilt
@@ -227,146 +326,84 @@ class Ui_Form(object):
         
         
     def started(self):
-        
-        #   Log file header
-        logger = "Time(UTC)\t\tZ-Channel Dip\tN-Channel Dip\tE-Channel Dip"
-        
-        #   Retrieve the 3 URLs
-        fullURL = [str(self.URLGrabZ.text()),
-                   str(self.URLGrabN.text()),
-                   str(self.URLGrabE.text())]
-        
-        #   Parse the front and back ends of the URL to single out the changing time
-        #   Will create a list of lists going z channel, n channel, and e channel
-        #   each sublist will be the front of the URL then the end of the URL
-        URLParse = [[fullURL[0][:fullURL[0].find('start=')],fullURL[0][(fullURL[0].rfind('&end=') + 24):]],
-                    [fullURL[1][:fullURL[1].find('start=')],fullURL[1][(fullURL[1].rfind('&end=') + 24):]],
-                    [fullURL[2][:fullURL[2].find('start=')],fullURL[2][(fullURL[2].rfind('&end=') + 24):]]]
-        
-        #   Get the channel names
-        chans = [(fullURL[0][fullURL[0].find('cha=')+4:])[0:3], #z channel
-                 (fullURL[1][fullURL[1].find('cha=')+4:])[0:3], #n channel
-                 (fullURL[2][fullURL[2].find('cha=')+4:])[0:3]] #e channel
-        
-        #   Get the current time of the button press
-        gettimeNow = datetime.datetime.now()-datetime.timedelta(seconds=1)
-
-        
-        #   Generate the time strings for the URL
-        #   Time is 1 second behind real time to allow for buffering
-        t_start = str((gettimeNow-datetime.timedelta(seconds=1)).date())+'T'+str((gettimeNow-datetime.timedelta(seconds=1)).time())
-        t = str(gettimeNow.date())+'T'+str(gettimeNow.time())
-        
-        #   Stop button setter
-        self.startstop = 1
-        
-        while self.startstop ==1:
-            #   Combine the string for the URL
-            timestring = 'start=' + t_start + '&end=' + t
-            
-            #   Make the stream variable
-            st = Stream()
-                  
-            #   Get data
-            for i in range(3):        
-                url = URLParse[i][0] + timestring + URLParse[i][1]
-                st.append(read(url)[0])
-                print(chans[i])
-        
-            
-            #   Get the mean for gravity from the channels
-            Zchan = np.mean(st.select(channel=chans[0])[0].data)
-            Nchan =  np.mean(st.select(channel=chans[1])[0].data)
-            Echan =  np.mean(st.select(channel=chans[2])[0].data)
-            QtWidgets.QApplication.processEvents()
-            
-            g = np.sqrt(Zchan**2+Nchan**2+Echan**2)
-
-            
-            # Get the current angles for each channel
-            alpha_N = np.arcsin(Nchan/g)*180/np.pi
-            alpha_E = np.arcsin(Echan/g)*180/np.pi
-            alpha_Z = np.arccos(Zchan/g)*180/np.pi
-            print(t + " Z: " + str(alpha_Z) + " N: " + str(alpha_N) + " E: " + str(alpha_E))
-            QtWidgets.QApplication.processEvents()
-            
-            #   Generate the tilt plot
-            #   Define the figure
-            self.ax1.clear()
-            self.ax2.clear()
-            self.ax3.clear()
-
-            #   Z-channel tiltmeter
-            self.ax1.plot(0,1)
-            self.ax1.arrow(float(alpha_Z)/180*np.pi,0,0,0.82,
-                                  width = 0.05, edgecolor = 'black', facecolor = 'blue', lw = 2, zorder = 2)
-            self.ax1.set_theta_direction(-1)
-            self.ax1.set_theta_zero_location("E")
-            self.ax1.set_thetamin(-90)
-            self.ax1.set_thetamax(90)
-            self.ax1.set_xticks(np.linspace((-1*(np.pi))/2,(np.pi)/2, 13))
-            self.ax1.set_yticklabels([])
-            self.ax1.set_title("Z-Channel Dip", pad = 20, size = 18)
-            self.ax1.set_xticklabels(['-90'+self.degS + '\n(Up)','-75'+self.degS,'-60'+self.degS,'-45'+self.degS,'-30'+self.degS,'-15'+self.degS,'0'+self.degS + "\n(Seafloor)",'15'+self.degS,'30'+self.degS,'45'+self.degS,'60'+self.degS,'75'+self.degS,'90'+self.degS +'\n(Down)'], size = 10)
-            
-            #   N-channel tiltmeter
-            self.ax2.plot(0,1)
-            self.ax2.arrow(float(alpha_N)/180*np.pi,0,0,0.82,
-                                  width = 0.05, edgecolor = 'black', facecolor = 'blue', lw = 2, zorder = 2)
-            self.ax2.set_theta_direction(-1)
-            self.ax2.set_theta_zero_location("E")
-            self.ax2.set_thetamin(-90)
-            self.ax2.set_thetamax(90)
-            self.ax2.set_xticks(np.linspace((-1*(np.pi))/2,(np.pi)/2, 13))
-            self.ax2.set_yticklabels([])
-            self.ax2.set_title("N-Channel Dip", pad = 20, size = 18)
-            self.ax2.set_xticklabels(['-90'+self.degS + '\n(Up)','-75'+self.degS,'-60'+self.degS,'-45'+self.degS,'-30'+self.degS,'-15'+self.degS,'0'+self.degS + "\n(Seafloor)",'15'+self.degS,'30'+self.degS,'45'+self.degS,'60'+self.degS,'75'+self.degS,'90'+self.degS +'\n(Down)'], size = 10)
-            
-            #   E-channel tiltmeter
-            self.ax3.plot(0,1)
-            self.ax3.arrow(float(alpha_E)/180*np.pi,0,0,0.82,
-                                 width = 0.05, edgecolor = 'black', facecolor = 'blue', lw = 2, zorder = 2)
-            self.ax3.set_theta_direction(-1)
-            self.ax3.set_theta_zero_location("E")
-            self.ax3.set_thetamin(-90)
-            self.ax3.set_thetamax(90)
-            self.ax3.set_xticks(np.linspace((-1*(np.pi))/2,(np.pi)/2, 13))
-            self.ax3.set_yticklabels([])
-            self.ax3.set_title("E-Channel Dip", pad = 20, size = 18)
-            self.ax3.set_xticklabels(['-90'+self.degS + '\n(Up)','-75'+self.degS,'-60'+self.degS,'-45'+self.degS,'-30'+self.degS,'-15'+self.degS,'0'+self.degS + "\n(Seafloor)",'15'+self.degS,'30'+self.degS,'45'+self.degS,'60'+self.degS,'75'+self.degS,'90'+self.degS +'\n(Down)'], size = 10)
-            
-            #   Draw the updates to the angle
-            self.ax1.figure.canvas.draw()
-            self.ax2.figure.canvas.draw()
-            self.ax3.figure.canvas.draw()
-            
-            #   Show the analog tilt update
-            self.xtilt.setText(str(round(alpha_Z,2)))
-            self.ytilt.setText(str(round(alpha_N,2)))
-            self.ztilt.setText(str(round(alpha_E,2)))
-            QtWidgets.QApplication.processEvents()
-            
-            
-            #   Draw the obspy figure
-            self.LiveWaveFigure.clf()
-            st.plot(fig = self.LiveWaveFigure, draw = True)
-            gettimeNow = datetime.datetime.now()-datetime.timedelta(seconds=1)
-
-            t_start = t
-            t = str(gettimeNow.date())+'T'+str(gettimeNow.time())
-            logger += "\n" + t + '\t' + str(round(alpha_Z,5)) + '\t' + str(round(alpha_N,5)) + '\t' + str(round(alpha_E,5))
-            QtWidgets.QApplication.processEvents()
-      
-        #   When the stream is cancelled, print a log of the dips
-        if self.startstop == 0:
-            with open('DipLog.txt','w') as t:
-                t.write(logger)
-            Form.close() #  Close the application
+        self.StartB.setEnabled(False)
+        self.startstop = True
+        self.worker_thread = GetLiveThread(self.startstop, self.URLGrabZ,self.URLGrabN,self.URLGrabE)
+        self.worker_thread.updateWave.connect(self.Plotter)
+        self.worker_thread.updateLogger.connect(self.Logger)
+        self.worker_thread.start()
 
     #   Button to press that stops the stream and gets the dip log
     def stopped(self):
-        self.startstop = 0
-        print(self.startstop)
+        self.StartB.setEnabled(True)
+        self.writer = True
+        self.worker_thread.terminate()
+    
+    def Plotter(self, stWave, alpha_Z, alpha_N, alpha_E):   
+        #   Generate the tilt plot
+        #   Define the figure
+        self.ax1.clear()
+        self.ax2.clear()
+        self.ax3.clear()
+
+        #   Z-channel tiltmeter
+        self.ax1.plot(0,1)
+        self.ax1.arrow(float(alpha_Z)/180*np.pi,0,0,0.82,
+                              width = 0.05, edgecolor = 'black', facecolor = 'blue', lw = 2, zorder = 2)
+        self.ax1.set_theta_direction(-1)
+        self.ax1.set_theta_zero_location("E")
+        self.ax1.set_thetamin(-15)
+        self.ax1.set_thetamax(15)
+        self.ax1.set_xticks(np.linspace((-1*(np.pi))/12,(np.pi)/12, 7))
+        self.ax1.set_yticklabels([])
+        self.ax1.set_title("Z-Channel Dip", pad = 20, size = 18)
+        self.ax1.set_xticklabels(['-15'+self.degS + ' (Up)','-10'+self.degS,'-5'+self.degS,'0'+self.degS,'5'+self.degS,'10'+self.degS,'15'+self.degS + ' (Down)'], size = 10)
+        
+        #   N-channel tiltmeter
+        self.ax2.plot(0,1)
+        self.ax2.arrow(float(alpha_N)/180*np.pi,0,0,0.82,
+                              width = 0.05, edgecolor = 'black', facecolor = 'blue', lw = 2, zorder = 2)
+        self.ax2.set_theta_direction(-1)
+        self.ax2.set_theta_zero_location("E")
+        self.ax2.set_thetamin(-15)
+        self.ax2.set_thetamax(15)
+        self.ax2.set_xticks(np.linspace((-1*(np.pi))/12,(np.pi)/12, 7))
+        self.ax2.set_yticklabels([])
+        self.ax2.set_title("N-Channel Dip", pad = 20, size = 18)
+        self.ax2.set_xticklabels(['-15'+self.degS + ' (Up)','-10'+self.degS,'-5'+self.degS,'0'+self.degS,'5'+self.degS,'10'+self.degS,'15'+self.degS + ' (Down)'], size = 10)
+        
+        #   E-channel tiltmeter
+        self.ax3.plot(0,1)
+        self.ax3.arrow(float(alpha_E)/180*np.pi,0,0,0.82,
+                             width = 0.05, edgecolor = 'black', facecolor = 'blue', lw = 2, zorder = 2)
+        self.ax3.set_theta_direction(-1)
+        self.ax3.set_theta_zero_location("E")
+        self.ax3.set_thetamin(-15)
+        self.ax3.set_thetamax(15)
+        self.ax3.set_xticks(np.linspace((-1*(np.pi))/12,(np.pi)/12, 7))
+        self.ax3.set_yticklabels([])
+        self.ax3.set_title("E-Channel Dip", pad = 20, size = 18)
+        self.ax3.set_xticklabels(['-15'+self.degS + ' (Up)','-10'+self.degS,'-5'+self.degS,'0'+self.degS,'5'+self.degS,'10'+self.degS,'15'+self.degS + ' (Down)'], size = 10)
+        
+        #   Draw the updates to the angle
+        self.ax1.figure.canvas.draw()
+        self.ax2.figure.canvas.draw()
+        self.ax3.figure.canvas.draw()
+        
+        #   Show the analog tilt update
+        self.xtilt.setText(str('%.4f' % round(alpha_Z,4)))
+        self.ytilt.setText(str('%.4f' % round(alpha_N,4)))
+        self.ztilt.setText(str('%.4f' % round(alpha_E,4)))
+        
+        #   Draw the obspy figure
+        self.LiveWaveFigure.clf()
+        stWave.plot(fig = self.LiveWaveFigure, draw = True)
+        
+        
+    def Logger(self, logger):
+        with open('DipLog.txt','w') as t:
+            t.write(logger)
+        
 
     #   Define base labels/titles
     def retranslateUi(self, Form):
